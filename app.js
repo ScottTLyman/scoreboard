a910a04455e8ed98f1c1f2e4046f80b12366c764
Scott T Lyman <s.t.lyman84@gmail.com>
2022-02-07 16:29:34 -0700

    basic counter display, counting  by 1

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let score = 0
+let totalT1 = 0;
+let totalT2 = 0;
 
 function scoreT1() {
   console.log("T1 SCORE!")
+  totalT1++
+  document.getElementById('totalT1').innerHTML = totalT1;
 }
+
 function scoreT2() {
   console.log(" T2 SCORE!")
+  totalT2++
+  document.getElementById('totalT2').innerHTML = totalT2;
 }
